1cefc685131a546aa365dff17277f4ecf7c742e3
Jacob Squier <jacob.squier@gmail.com>
2020-06-23 02:16:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,6 +7,7 @@ def my_hash
   hash = {
     :name "Jake", :age "13"
   }
+  return hash
 end
 
 def pioneer
@@ -15,5 +16,5 @@ end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  hash = { :id number }
 end
